86a9e52c85f644b1e83d4cccff8bc72ae352d890
Ranjeet Dev <thisisranjeetk@gmail.com>
2016-06-07 16:45:21 +0530

    project is uploaded

---
+++ App/Product/Controller/ProductController.js
@@ -0,0 +1,66 @@
+newApp.controller('ProductCtrl', ['$scope','$http',
+    function ($scope, $http) {
+        debugger;
+		$scope.images = [{
+			url: "Image/gp1.png"
+		}
+		,{
+			url: "Image/gp2.jpg"
+			
+		}
+		,{
+				url: "Image/gp3.jpg"
+		}];
+		$scope.changeBackground = function(url){
+			
+			$('.pdhead').css('background-image','url(' + url + ')');
+		}
+
+
+
+
+			$scope.myDate = new Date();
+			$scope.convertToDate = function (stringDate) {
+				var dateOut = new Date(stringDate);
+				dateOut.setDate(dateOut.getDate() + 1);
+				return dateOut;
+			};
+
+
+		$scope.urls = [{
+			url: "Image/Facebook.png"
+		}
+			,{
+				url: "Image/Twitter.png"
+
+			}
+			];
+
+		$scope.changeurl = function(url) {
+
+
+		}
+		$scope.tabs = [{
+			title: 'About The Class',
+			url: 'one.tpl.html'
+		}, {
+			title: 'Venue',
+			url: 'two.tpl.html'
+		}, {
+			title: 'T & C',
+			url: 'three.tpl.html'
+		}];
+
+		$scope.currentTab = 'one.tpl.html';
+
+		$scope.onClickTab = function (tab) {
+			$scope.currentTab = tab.url;
+		}
+
+		$scope.isActiveTab = function(tabUrl) {
+			return tabUrl == $scope.currentTab;
+		};
+		$scope.textPrd=" A classroom is a learning space, a room in which classes are held" + "Classrooms are found in educational institutions of all kinds";
+}]);
+
+
---
+++ App/app.js
@@ -0,0 +1,45 @@
+﻿var newApp = angular.module('myApp', [ 'ngRoute','ng-bootstrap-datepicker' ]);
+
+newApp.config(function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+        .when('/product', {
+            templateUrl : 'App/Product/View/product.html',
+            controller : 'ProductCtrl'
+        })
+        .when('/Register', {
+            templateUrl: 'App/RegisterNow/View/RegisterNow.html',
+            controller : 'RegisterCtrl'
+        })
+        .when('/BecomeACoach', {
+            templateUrl : 'App/BecomeACoach/View/BecomeACoach.html',
+            controller : 'BecomeACoachCtrl'
+        })
+        .when('/analytics', {
+            templateUrl : 'partials/analytics.html',
+            controller : 'analyticsCtrl'
+        })
+        .when('/catagories', {
+            templateUrl : 'partials/categories.html',
+            controller : 'categoriesCtrl'
+
+        }).when('/AdminLogin', {
+        templateUrl : 'partials/login.html',
+        controller : 'AdminloginCtrl'
+
+    }).when('/Logout', {
+        templateUrl : 'partials/AdminLogin.html',
+        controller : 'AdminlogoutCtrl'
+
+    }).when('/dashboard', {
+        templateUrl : 'partials/OrganizationDashboard.html',
+        controller : 'BreadCrumbCtrl'
+
+    }).otherwise({
+        redirectTo : '/product'
+    });
+    // use the HTML5 History API
+    //$locationProvider.html5Mode(true);
+});
+
